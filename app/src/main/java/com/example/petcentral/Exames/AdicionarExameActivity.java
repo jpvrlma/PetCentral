3527bb2ede66c8f73dbbe9653c49daacc4360acc
Joao Pedro <joao@users.github.com>
2024-09-15 00:50:01 -0300

    ARQUIVOS

--- app/src/main/java/com/example/petcentral/Exames/AdicionarExameActivity.java
+++ app/src/main/java/com/example/petcentral/Exames/AdicionarExameActivity.java
@@ -1,10 +1,17 @@
 package com.example.petcentral.Exames;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,6 +27,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class AdicionarExameActivity extends AppCompatActivity {
 
     private ActivityAdicionarExameBinding binding;
+    private Uri fileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +59,59 @@ public class AdicionarExameActivity extends AppCompatActivity {
             View view1 = LayoutInflater.from(this).inflate(R.layout.bottom_exames, null);
             bottomSheetDialog.setContentView(view1);
             bottomSheetDialog.show();
+
+            TextView tvPdf = view1.findViewById(R.id.tv_pdf);
+
+            tvPdf.setOnClickListener(v1 -> {
+                selecionarArquivo();
+            });
         });
     }
+
+
+    private void selecionarArquivo(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        filePicker.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> filePicker = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == AppCompatActivity.RESULT_OK && result.getData() != null) {
+                    fileUri = result.getData().getData();
+
+                    if (fileUri != null){
+                       String nome = getFileName(fileUri);
+                       binding.tvArquivosSelecionados.setText(nome);
+                    }
+                }
+            });
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (index >= 0) {
+                        result = cursor.getString(index);
+                    }}
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
 
 }
